c028af7a67c92e017c8b1ee71a246008b54bf43e
Tony Messias <tonysm@hey.com>
2024-05-10 14:43:36 -0300

    wip

--- app/Models/Room.php
+++ app/Models/Room.php
@@ -8,7 +8,6 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\HasMany;
-use Illuminate\Support\Arr;
 use Illuminate\Support\Facades\DB;
 use Parental\HasChildren;
 
